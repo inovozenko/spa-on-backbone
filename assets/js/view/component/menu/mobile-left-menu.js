4ff0f794d4cc990865823cfa59f385db9148d9d5
Игорь Новозенко <inovozenko@intervale.ru>
2016-03-15 13:45:56 +0300

    Menu always on top. Made mobile menu clickable.

--- assets/js/view/component/menu/mobile-left-menu.js
+++ assets/js/view/component/menu/mobile-left-menu.js
@@ -15,15 +15,16 @@ yp.v.MobileLeftMenu = Backbone.View.extend({
     },
 
     clickItem: function (event) {
-        var href = $(event.target).attr('href');
+        var href = $(event.currentTarget).attr('href');
 
         if (href.slice(0, 1) === '#') { //  if the clicked link is linked to an anchor, scroll the page to that anchor
-            this.$el.one('closed.mm', function() {
-                setTimeout(function() {
-                    $('html, body').animate({
-                        scrollTop: $(href).offset().top
-                    });
-                }, 10);
+            var mMenuAPI = this.$el.data('mmenu');
+            mMenuAPI.bind('closed', function() {
+                var mMenu = mMenuAPI.getInstance();
+                delete mMenu.cbck.closed; // hard-code of unbind
+                $('html, body').animate({
+                    scrollTop: $(href).offset().top
+                }, 1500, 'easeInOutExpo');
             });
         }
     }
